f67948dd3ebafcc637dc3bb0cb33aace43088b9d
pooriaAcademy <pooria.poorsarvi.tehrani@gmail.com>
2021-05-26 01:12:14 -0400

    util file updated

--- main.go
+++ main.go
@@ -6,21 +6,6 @@ import (
 	"net/http"
 )
 
-func CORSCheck(handler func (w http.ResponseWriter, req *http.Request)) func (w http.ResponseWriter, req *http.Request){
-	res := func (w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Methods", "*")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Max-Age", "3600")
-		if req.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
-		handler(w, req)
-	}
-	return res
-}
 
 func init() {
 	stripe.Key = "sk_test_51IoMpOKNyEKTIrMReXkfEIsEZhTRIspduEAD7KzNhNgDn2A8MpWeQsxXxuNmcD5jEumM6eUnQAIUKIjOBCDmGmt600nMiLC9JI"
